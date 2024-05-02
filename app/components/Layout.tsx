fbd7d1ba1dbae9631149f1f3dac9bcc7890b8d83
sumit.sharma <sumit.sharma@cloudactivelabs.com>
2024-05-02 15:20:25 +0530

    header changes

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -260,11 +260,7 @@ function DesktopHeader({
   return (
     <header
       role="banner"
-      className={`${
-        isHome
-          ? 'bg-primary/80 dark:bg-contrast/60 text-contrast dark:text-primary shadow-darkHeader'
-          : 'bg-contrast/80 text-primary'
-      } ${
+      className={`${'bg-contrast/80 text-primary'} ${
         !isHome && y > 50 && ' shadow-lightHeader'
       } hidden h-nav lg:flex items-center sticky transition duration-300 backdrop-blur-lg z-40 top-0 justify-between w-full leading-none gap-8 px-12 py-8`}
     >
